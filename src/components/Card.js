e0fc8f355384f801ed26f363c49c19505a78a6ba
VEBata <vkhloponya@yandex.ru>
2023-07-06 01:02:25 +0300

    push17

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,22 +4,22 @@ const Card = ({ card, onCardClick }) => {
   };
 
   return (
-<div className="element">
-  <img 
-  className="element__img" 
-  src={card.link}
-  alt={card.name}
-  onClick={handleCardClick}
-  />
-  <div className="element__item">
-    <h2 className="element__text">{card.name}</h2>
-    <div className="element__likes-info">
-      <button type="button" className="element__button" />
-      <p className="element__counter-likes">{card.likes.length}</p>
+    <div className="element">
+      <img
+        className="element__img"
+        src={card.link}
+        alt={card.name}
+        onClick={handleCardClick}
+      />
+      <div className="element__item">
+        <h2 className="element__text">{card.name}</h2>
+        <div className="element__likes-info">
+          <button type="button" className="element__button" />
+          <p className="element__counter-likes">{card.likes.length}</p>
+        </div>
+      </div>
+      <button className="element__button_basket" type="button" />
     </div>
-  </div>
-  <button className="element__button_basket" type="button" />
-</div>
   );
 };
 
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,20 +1,12 @@
 const ImagePopup = ({ card, isOpen, onClose }) => {
   return (
-<div className={`popup popup-full" ${isOpen ? "popup_opened" : ""}`}>
-  <div className="popup__container-full">
-    <button 
-    className="popup__close popup-full__close"
-    onClick={onClose}
-    />
-    <img 
-    className="popup__img-full" 
-    src={card.link}
-    alt={card.name}
-    />
-    <p 
-    className="popup__heading-full">{card.name}</p>
-  </div>
-</div>
+    <div className={`popup popup-full" ${isOpen ? "popup_opened" : ""}`}>
+      <div className="popup__container-full">
+        <button className="popup__close popup-full__close" onClick={onClose} />
+        <img className="popup__img-full" src={card.link} alt={card.name} />
+        <p className="popup__heading-full">{card.name}</p>
+      </div>
+    </div>
   );
 };
 export default ImagePopup;
