c25e1932f51a321595e178f9244bd31b5f31c16e
VEBata <vkhloponya@yandex.ru>
2023-08-05 22:33:27 +0300

    поправил валидацию

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,81 +1,73 @@
 class Api {
-  constructor(options) {
-    this._url = options.url;
-    this._headers = options.headers;
-  }
+	constructor(options) {
+		this._url = options.url;
+		this._headers = options.headers;
+	}
 
-  //проверка на ошибки
-  _checkResponse(res) {
-    if (!res.ok) {
-      return Promise.reject(`Ошибка: ${res.status}`);
-    }
-    return res.json();
-  }
+	_request(endpoint, options) {
+		return fetch(this._url + endpoint, options).then(this._handleResponse)
+	}
 
-  //запрос данных с сервера
-  getInitialsCards() {
-    return fetch(`${this._url}/cards`, {
-      method: "GET",
-      headers: this._headers,
-    }).then(this._checkResponse);
-  }
+	_handleResponse(res) {
+		if (res.ok) {
+			return res.json();
+		}
+		return Promise.reject(`handleResponse - ошибка: ${res.status}`);
+	}
 
-  getDataUser() {
-    return fetch(`${this._url}/users/me`, {
-      headers: this._headers,
-    }).then(this._checkResponse);
-  }
+	getDataCards() {
+		return this._request('/cards', { headers: this._headers })
+	}
 
-  setUserData(data) {
-    return fetch(`${this._url}/users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        name: data.name,
-        about: data.about,
-      }),
-    }).then(this._checkResponse);
-  }
+	getDataUser() {
+		return this._request('/users/me', { headers: this._headers })
+	}
 
-  setUserAvatar(data) {
-    return fetch(`${this._url}/users/me/avatar`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify({
-        avatar: data.avatar,
-      }),
-    }).then(this._checkResponse);
-  }
-  //лайк-дизлайк
-  toggleLike(cardId, isLiked) {
-    return fetch(`${this._url}/cards/${cardId}/likes`, {
-      headers: this._headers,
-      method: isLiked ? "DELETE" : "PUT",
-    }).then(this._checkResponse);
-  }
+	setDataUser(data) {
+		return this._request('/users/me', {
+			method: 'PATCH',
+			headers: this._headers,
+			body: JSON.stringify({
+				name: data.name,
+				about: data.about })
+		})
+	}
 
-  // Добавление новой карточки в галерею
-  addNewPhotocard(name, link) {
-    return fetch(`${this._url}/cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify({ name, link }),
-    }).then(this._checkResponse);
-  }
+	setUserAvatar(avatar) {
+		return this._request('/users/me/avatar', {
+			method: 'PATCH',
+			headers: this._headers,
+			body: JSON.stringify(avatar)
+		})
+	}
 
-  //удаление карточки
-  deleteCard(cardId) {
-    return fetch(`${this._url}/cards/${cardId}`, {
-      method: "DELETE",
-      headers: this._headers,
-    }).then(this._checkResponse);
-  }
+	addNewCard(card) {
+		return this._request('/cards', {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify(card)
+		})
+	}
+
+	deleteCard(cardId) {
+		return this._request(`/cards/${cardId}`, {
+			method: 'DELETE',
+			headers: this._headers
+		})
+	}
+
+	changeLikeCardStatus(cardId, isLiked) {
+		return this._request(`/cards/${cardId}/likes`, {
+			method: isLiked ? "DELETE" : "PUT",
+			headers: this._headers,
+		})
+	}
 }
 
 export const api = new Api({
-  url: "https://mesto.nomoreparties.co/v1/cohort-68",
-  headers: {
-    authorization: "e0cc1650-18be-49f9-89e7-3eeb4201f8b2",
-    "Content-Type": "application/json",
-  },
-});
+	url: 'https://mesto.nomoreparties.co/v1/cohort-68',
+	headers: {
+		authorization: 'e0cc1650-18be-49f9-89e7-3eeb4201f8b2',
+		'Content-Type': 'application/json',
+	}
+})
